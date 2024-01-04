9d77c137ef82ccdf019f67eadae0f44caf691bee
NicolasLOrtiz <contato.nicolol@gmail.com>
2024-01-03 22:25:36 -0300

    Configurando Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {ThemeProvider} from "styled-components";
+import {defaultTheme} from "./styles/themes/default.ts";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ ThemeProvider>
   )
 }
 
