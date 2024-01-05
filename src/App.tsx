9c0128a6c025bdfc7388543bdcb2a175c1d99a32
NicolasLOrtiz <contato.nicolol@gmail.com>
2024-01-05 20:59:36 -0300

    Estilos globais e ESlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {ThemeProvider} from "styled-components";
-import {defaultTheme} from "./styles/themes/default.ts";
-import {GlobalStyle} from "./styles/global.ts";
+import viteLogo from '../../../../vite.svg'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global.ts'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +11,10 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <GlobalStyle />
-    </ ThemeProvider>
+    </ThemeProvider>
   )
 }
 
